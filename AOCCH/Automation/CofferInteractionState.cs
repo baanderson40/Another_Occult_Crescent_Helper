ca629b4768e6c8975ca75e36fd59605237b6aeff
baanderson40 <baanderson40@gmail.com>
2026-08-10 18:27:06 -0500

    Reposition before pot coffer retries

--- AOCCH/Automation/CofferInteractionState.cs
+++ AOCCH/Automation/CofferInteractionState.cs
@@ -7,6 +7,8 @@ public enum CofferInteractionState
     TargetingCoffer,
     InteractingWithCoffer,
     WaitingForOpenConfirmation,
+    RepositioningPastCoffer,
+    ReturningToCoffer,
     Opened,
     LostCoffer,
     TimedOut,
